d45009f99d4e4ede915f0ba9c8675103ac7f501b
jyotishankar04 <jyotishankar@gmail.com>
2026-06-25 14:07:57 +0530

    added zod validator

--- backend/src/modules/auth/auth.route.ts
+++ backend/src/modules/auth/auth.route.ts
@@ -1,8 +1,19 @@
 import { Router } from "express";
+import { userRegisterSchema } from "./auth.validator";
 
 const router: Router = Router();
 
 router.get("/", (_req, res) => {
+  const body = _req.body;
+
+  // const validator = userRegisterSchema.safeParse(body);
+  // if (!validator.success) {
+  //   return res.status(400).json({
+  //     success: false,
+  //     message: validator.error,
+  //   });
+  // }
+
   res.json({
     success: true,
     message: "Auth route is working",
